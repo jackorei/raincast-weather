d8f5b9d003c3652ee562940c7850f9544ba7d4fe
foxstacks <198858136+foxstacks@users.noreply.github.com>
2025-05-30 13:54:58 -0500

    top container shows some weather data. ui updated.

--- src/dom.js
+++ src/dom.js
@@ -2,12 +2,49 @@ import { weatherData } from "./weather";
 
 const searchform = document.querySelector('#searchform')
 const weathersearch = document.querySelector('#weathersearch')
+const temptext = document.querySelector('#temp')
+const conditiontext = document.querySelector('#conditions')
+const topleft = document.querySelector('.top-left')
+const location = document.querySelector('#locationtitle')
+const loading = document.querySelector('.fulldiv')
+const uv = document.querySelector('#uv')
+const wind = document.querySelector('#wind')
 
-searchform.addEventListener('submit', (event) => {
+async function startWeather() {
+    const data = await weatherData('Chicago')
+    if (data) {
+        topleft.style.display = 'flex'
+        const displaytemp = `${Math.round(data.temperature)}°F`
+        temptext.textContent = displaytemp
+        conditiontext.textContent = data.condition
+        location.textContent = data.location
+        uv.textContent = `UV Index: ${data.uvindex}`
+        wind.textContent = `Wind Speed: ${data.wind} mph`
+        
+        console.log(data)
+    }
+}
+startWeather()
+
+searchform.addEventListener('submit', async (event) => {
     event.preventDefault()
+    loading.style.display = 'flex'
     if (weathersearch.value === '') {
         console.log('Invalid search.')
     }
     const formsearch = weathersearch.value
-    weatherData(formsearch)
+    const data = await weatherData(formsearch)
+    if (data) {
+        loading.style.display = 'none'
+        const displaytemp = `${Math.round(data.temperature)}°F`
+        temptext.textContent = displaytemp
+        conditiontext.textContent = data.condition
+        location.textContent = data.location
+        uv.textContent = `UV Index: ${data.uvindex}`
+        wind.textContent = `Wind Speed: ${data.wind} mph`
+        console.log(data)
+        
+    }
+    
 })
+
--- src/weather.js
+++ src/weather.js
@@ -1,21 +1,22 @@
-export async function weatherData(search) {
-    try {
-        const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${search}?unitGroup=us&key=BDH93GK7EJBZUC4QNBKK6FPTP&contentType=json`)
-        const data = await response.json()
-        console.log(myData(data))
-    }
-    catch(error) {
-        console.log('An error occurred: ', error)
-    }
-}
-
 export function myData(data) {
     return {
-        location: data.address,
+        location: data.resolvedAddress,
         temperature: data.currentConditions.temp,
         uvindex: data.currentConditions.uvindex,
         condition: data.currentConditions.conditions,
         icon: data.currentConditions.icon,
         wind: data.currentConditions.windspeed,
+    }
+}
+
+export async function weatherData(search) {
+    try {
+        const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${search}?unitGroup=us&key=BDH93GK7EJBZUC4QNBKK6FPTP&contentType=json`)
+        const data = await response.json()
+        console.log(data)
+        return myData(data)
+    }
+    catch(error) {
+        console.log('An error occurred: ', error)
     }
 }
